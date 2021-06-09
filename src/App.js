b87a6de5df05a95c26379f860320d62b2e72c5cf
Chris Cooley <68765420+chriscooley1@users.noreply.github.com>
2021-06-08 21:07:11 -0600

    cleaned up App.js and added name, email, birthdate and submit button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>Contact Us</p>
+      <div>
+        <form action="/action_page.php">
+          <label>Name</label>
+          <input type="text" id="name" name="name" placeholder="Name" />
+          
+          <label>Email</label>
+          <input type="email" id="email" name="email" placeholder="Email" />
+
+          <label>Birth date</label>
+          <input type="birth date" name="birth date" placeholder="Birth date" />
+
+          <label>I agree to be contacted via email</label>
+          <input type="submit" value="Clear" />
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+      </div>
+    );
+  }
 }
 
 export default App;
